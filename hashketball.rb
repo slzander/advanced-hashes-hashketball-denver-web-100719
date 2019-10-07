ab8f57f08b763f594fe593363e3c78add0a1e5c4
Stacey Zander <staceyzander@gmail.com>
2019-10-07 20:53:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 def game_hash
- hash = {:home => {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => [
+    {:home => {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => [
    {:name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
    {:name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7}, 
    {:name => "Brook Lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15},
